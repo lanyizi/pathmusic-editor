fb1de17505ff891dd1733d0c6ed87c8b1f4484c1
lanyizi <lanyi@ra3.moe>
2023-05-16 11:14:33 +0200

    start working on views

---
+++ src/components/BrowserFileProvider.vue
@@ -0,0 +1,77 @@
+<template>
+  <div
+    class="drop-zone"
+    ref="dropZone"
+    @dragover="handleDragOver"
+    @drop="handleFileDrop"
+  >
+    Drop files here<br />
+    Required files:
+    <ul>
+      <li v-for="file in requiredFileList" :key="file">
+        {{ file }}
+      </li>
+    </ul>
+    Available files:
+    <ul>
+      <li v-for="file in availableFilesList" :key="file.path">
+        {{ file.path }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useFileStore, type PendingTask } from '@/file-store';
+import { computed } from 'vue';
+
+const dropZone = ref<HTMLElement>(null as unknown as HTMLElement);
+const { requestedTextFiles, requestedBinaryFiles } = useFileStore();
+const requiredFileList = computed(() =>
+  (requestedTextFiles.value as PendingTask<any>[])
+    .concat(requestedBinaryFiles.value)
+    .map((file) => file.path)
+);
+interface AvailableFile {
+  path: string;
+  file: File;
+}
+const availableFilesList = ref<AvailableFile[]>([]);
+
+function handleDragOver(event: DragEvent) {
+  event.preventDefault();
+  // Add CSS styles to indicate the drop zone is active
+  dropZone.value.classList.add('drag-over');
+}
+
+function handleFileDrop(event: DragEvent) {
+  event.preventDefault();
+  // Remove CSS styles indicating active drop zone
+  dropZone.value.classList.remove('drag-over');
+
+  const files = event.dataTransfer?.files;
+  if (files) {
+    // Process the dropped files (e.g., upload, preview, etc.)
+    handleFiles(files);
+  }
+}
+
+function handleFiles(files: FileList) {
+  // Process each file individually
+  for (const file of files) {
+    // Perform necessary operations (e.g., upload, read, etc.)
+    availableFilesList.value = [
+      ...availableFilesList.value,
+      {
+        path: file.name,
+        file,
+      },
+    ];
+  }
+}
+</script>
+<style scoped>
+.drag-over {
+  background-color: var(--color-background-soft);
+}
+</style>
---
+++ src/views/EditorView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Hi!</div>
+  <BrowserFileProvider></BrowserFileProvider>
+</template>
+<script setup lang="ts">
+import BrowserFileProvider from '@/components/BrowserFileProvider.vue';
+</script>
