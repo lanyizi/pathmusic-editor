08c62d2b899120cd49f7d9a8899d2b85c9b8a9a4
lanyizi <lanyi@ra3.moe>
2023-05-17 18:08:14 +0200

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div class="container">
-    <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </header>
     <RouterView class="content" />
   </div>
 </template>
@@ -22,36 +16,6 @@ html {
 .container {
   height: 100vh;
   display: grid;
-  grid-template-rows: min-content minmax(0, 1fr);
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-  grid-row: 1;
-}
-
-.content {
-  grid-row: 2;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
+  grid-template-rows: minmax(0, 1fr);
 }
 </style>
