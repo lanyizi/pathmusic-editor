c30f29b773807f0fd712a9368d0bb7f6c9cca48c
lanyizi <lanyi@ra3.moe>
2023-05-19 09:15:36 +0200

    event list

---
+++ src/components/EventList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ol>
+    <li v-for="event in model.data.events" :key="event.name">
+      <RouterLink :to="{ query: createQuery('event', event.id) }">
+        {{ event.name }}
+      </RouterLink>
+    </li>
+  </ol>
+</template>
+<script setup lang="ts">
+import { modelKey } from '@/model';
+import { createQuery } from '@/router/create-query';
+import { inject } from 'vue';
+
+const model = inject(modelKey);
+if (!model) {
+  throw new Error('model is not provided');
+}
+</script>
